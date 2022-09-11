2c0c4b3283787e5cf69a26c603155fde30f962a2
Jeffrey Sun <telljsun@gmail.com>
2022-09-11 16:47:02 -0700

    Initial Commit

---
+++ Shared/TradingDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingDemoApp.swift
+//  Shared
+//
+//  Created by Jeffrey Sun on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct TradingDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
