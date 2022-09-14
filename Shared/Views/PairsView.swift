f8ecd82c9cc19090fa045d788fee4360920d741e
Jeffrey Sun <telljsun@gmail.com>
2022-09-13 21:49:55 -0700

    remove macOS target

---
+++ Shared/Views/PairsView.swift
@@ -0,0 +1,20 @@
+//
+//  PairsView.swift
+//  TradingDemo (iOS)
+//
+//  Created by Jeffrey Sun on 9/13/22.
+//
+
+import SwiftUI
+
+struct PairsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PairsView_Previews: PreviewProvider {
+    static var previews: some View {
+        PairsView()
+    }
+}
